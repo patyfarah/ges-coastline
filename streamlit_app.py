26c992ff3e00ce6b4f6f72c4240063d2c5bed98d
Patty Farah <118547769+patyfarah@users.noreply.github.com>
2025-05-24 21:48:16 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,8 @@ ee.Initialize(credentials)
 ges_params1 = {
     'bands': ['GES'],
     'palette': ['#a50026', '#f88d52', '#ffffbf', '#86cb66', '#006837'],
-    'min': -100,
-    'max': 25,
+    'min': -25,
+    'max': 75,
     'labels': ['Very Severe', 'Severe', 'No Change', 'Good Environmental', 'Excellent Improvement']
 }
 
