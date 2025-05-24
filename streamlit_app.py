6bc404776fdd8f2b3a02235d05614363ac449819
Patty Farah <118547769+patyfarah@users.noreply.github.com>
2025-05-24 22:49:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -187,7 +187,7 @@ if st.button("Run Analysis"):
     # Create and display the map below the title 
 
     m = geemap.Map()
-    m.setOptions(dragging=False, attributionControl=False)
+    m.setOptions( attributionControl=False)
     m.centerObject(region, 6)
     m.addLayer(GES_first, ges_params1, "GES Start Year",shown=False)
     m.addLayer(GES_last, ges_params1, "GES End Year",shown=False)
