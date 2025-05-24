5c9c19fa55647d209a0c646ef94c2c6e38c110d4
Patty Farah <118547769+patyfarah@users.noreply.github.com>
2025-05-24 09:42:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
+#-------------------------------------------------------
+# Libraries
+#-------------------------------------------------------
 import streamlit as st
 import geopandas as gpd
 import ee
 import geemap.foliumap as geemap
 import folium
+from google.oauth2 import service_account
 import gc
 
 #--------------------------------------------------------
