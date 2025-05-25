758f4000ae0b9fcefcb6f72e9acfc2802f7949ef
Patty Farah <118547769+patyfarah@users.noreply.github.com>
2025-05-25 09:29:06 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -213,7 +213,10 @@ if st.button("Run Analysis"):
         GES_diff = GES_last.subtract(GES_first)
         
         # Display the map
-        m = geemap.Map(draw_ctrl=False)
+        m = geemap.Map()
+        m.setControlVisibility({
+          drawingToolsControl:false
+        })
         m.centerObject(region, 6)
         m.addLayer(GES_first, ges_params1, "GES Start Year", shown=False)
         m.addLayer(GES_last, ges_params1, "GES End Year", shown=False)
